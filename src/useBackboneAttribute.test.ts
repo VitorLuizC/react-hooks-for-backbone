b51db5fd24db7f42d489b67e3fc6178979bf8695
Vitor L Cavalcanti <vitorluizc@outlook.com>
2022-08-01 16:41:23 -0300

    :sparkles: create useBackboneAttribute hook

---
+++ src/useBackboneAttribute.test.ts
@@ -0,0 +1,74 @@
+import { act, renderHook } from '@testing-library/react';
+import { Model } from 'backbone';
+import useBackboneAttribute from './useBackboneAttribute';
+
+type CarModelAttributes = {
+  brand: string;
+  model: string;
+};
+
+type CarModel = Model<CarModelAttributes>;
+
+describe('useBackboneAttribute', () => {
+  const car: CarModel = new Model({
+    brand: 'Ferrari',
+    model: 'Enzo',
+  });
+
+  it("returns model's attribute value", () => {
+    const { result } = renderHook(() => {
+      return useBackboneAttribute({
+        key: 'brand',
+        model: car,
+      });
+    });
+
+    expect(result.current[0]).toBe('Ferrari');
+  });
+
+  it("returns a function to update model's attribute", () => {
+    const { result } = renderHook(() => {
+      return useBackboneAttribute({
+        key: 'brand',
+        model: car,
+      });
+    });
+
+    const setBrand = result.current[1];
+
+    act(() => setBrand('Porsche'));
+
+    expect(result.current[0]).toBe('Porsche');
+    expect(car.get('brand')).toBe('Porsche');
+
+    act(() => setBrand((currentBrand) => currentBrand + '®'));
+
+    expect(result.current[0]).toBe('Porsche®');
+    expect(car.get('brand')).toBe('Porsche®');
+  });
+
+  describe('when watching events', () => {
+    it("updates value when model's events are triggered", () => {
+      const { result } = renderHook(() => {
+        return useBackboneAttribute({
+          key: 'model',
+          model: car,
+          watch: {
+            events: ['rename'],
+          },
+        });
+      });
+
+      car.set('model', '911');
+
+      // Value only updates when using 'setValue' or triggering events
+      expect(result.current[0]).toBe('Enzo');
+
+      act(() => {
+        car.trigger('rename');
+      });
+
+      expect(result.current[0]).toBe('911');
+    });
+  });
+});
---
+++ src/useBackboneAttribute.ts
@@ -0,0 +1,133 @@
+import type { Model, ModelSetOptions } from 'backbone';
+import { useCallback, useMemo } from 'react';
+import useBackboneEventListener from './useBackboneEventListener.js';
+import useUpdate from './utils/useUpdate.js';
+
+/**
+ * Function that receives two arguments: The first can be both the new
+ * attribute value, or a function that receives previous attribute value and
+ * returns the new one; and the second one is the Models' set options.
+ *
+ * @example
+ *
+ * ```ts
+ * let setUserName: BackboneAttributeUpdate<
+ *   UserAttributes,
+ *   'name',
+ * >;
+ *
+ * setUserName('Unkown', {
+ *   validate: true,
+ * });
+ *
+ * // or
+ *
+ * setUserName((previousName) => previousName?.trim() ?? 'Uknown', {
+ *   validate: true,
+ * });
+ * ```
+ */
+type BackboneAttributeUpdate<
+  TAttributes,
+  TKey extends string & keyof TAttributes,
+  TOptions = ModelSetOptions,
+> = (
+  newValueOrFunction:
+    | TAttributes[TKey]
+    | ((value: NonNullable<TAttributes[TKey]> | null) => TAttributes[TKey]),
+  options?: TOptions,
+) => void;
+
+/**
+ * Object that contains the attribute key, its model and a list of events to
+ * watch in order to keep its value synchronized.
+ *
+ * @example
+ *
+ * ```ts
+ * const options = {
+ *   key: 'name',
+ *   model: user,
+ *   watch: {
+ *     events: ['rename', 'change:name'],
+ *   },
+ * } as BackboneAttributeOptions<UserAttributes, 'name'>;
+ * ```
+ */
+export type BackboneAttributeOptions<
+  TAttributes,
+  TKey extends string & keyof TAttributes,
+  TOptions = ModelSetOptions,
+> = {
+  key: TKey;
+  model: Model<TAttributes, TOptions>;
+  watch?: {
+    events?: string[];
+  };
+};
+
+/**
+ * React.js Hook that provides the Backbone Models' attribute value and a
+ * function to update it. It also watches received events to keep its value
+ * synchronized with the model.
+ *
+ * @example
+ *
+ * ```ts
+ * const [name, setName] = useBackboneAttribute({
+ *   key: 'name',
+ *   model: user,
+ *   watch: {
+ *     events: ['change:name', 'sync', 'rename'],
+ *   },
+ * });
+ *
+ *
+ * const handleChange = (event: KeyboardEvent<HTMLInputElement>) => {
+ *   setName(event.target.value);
+ * };
+ *
+ * return <input value={name} onChange={handleChange} />;
+ * ```
+ */
+function useBackboneAttribute<
+  TAttributes,
+  TKey extends string & keyof TAttributes,
+  TOptions = ModelSetOptions,
+>(
+  options: BackboneAttributeOptions<TAttributes, TKey, TOptions>,
+): [
+  value: NonNullable<TAttributes[TKey]> | null,
+  setValue: BackboneAttributeUpdate<TAttributes, TKey, TOptions>,
+] {
+  const { key, model, watch: { events = [] } = {} } = options;
+
+  const getValue = useCallback(() => model.get(key) ?? null, [key, model]);
+
+  const [updateId, update] = useUpdate();
+
+  useBackboneEventListener(model, events, update);
+
+  const value = useMemo(getValue, [getValue, updateId]);
+
+  type SetValue = BackboneAttributeUpdate<TAttributes, TKey, TOptions>;
+
+  const setValue = useCallback<SetValue>(
+    (newValueOrFunction, options) => {
+      const newValue =
+        typeof newValueOrFunction !== 'function'
+          ? newValueOrFunction
+          : // @ts-expect-error because attribute's value could be a function.
+            newValueOrFunction(getValue());
+
+      model.set(key, newValue, options);
+
+      update();
+    },
+    [key, model, getValue],
+  );
+
+  return [value, setValue];
+}
+
+export default useBackboneAttribute;
