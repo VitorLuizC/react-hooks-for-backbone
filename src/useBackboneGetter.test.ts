7d6cf3f90bfe556a25ab12bb888301b53f328a94
Vitor L Cavalcanti <vitorluizc@outlook.com>
2022-08-01 15:17:51 -0300

    :white_check_mark: add tests for useBackboneGetter

--- src/useBackboneGetter.test.ts
+++ src/useBackboneGetter.test.ts
@@ -1,6 +1,6 @@
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { Model } from 'backbone';
-import useBackboneGetter from './useBackboneGetter';
+import useBackboneGetter from './useBackboneGetter.js';
 
 type User = Model<{
   firstName: string;
@@ -8,31 +8,108 @@ type User = Model<{
 }>;
 
 describe('useBackboneGetter', () => {
-  const getFullName = (user: User) => {
-    const firstName = user.get('firstName');
-    const lastName = user.get('lastName');
+  const user: User = new Model({
+    firstName: 'Carlos',
+    lastName: 'Marcos',
+  });
 
-    if (!firstName) return 'Unknown';
+  const getFullName = (user: User): string => {
+    /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
-    if (!lastName) return firstName;
-
+    const firstName = user.get('firstName')!;
+    const lastName = user.get('lastName')!;
     return firstName.concat(' ', lastName);
   };
 
-  describe('when is the first render', () => {
-    it('returns retult using getter function', () => {
-      const user: User = new Model({
-        firstName: 'Karl',
-        lastName: 'Marx',
-      });
+  describe('when first renders', () => {
+    it('returns result using getter function', () => {
+      const getter = jest.fn(getFullName);
 
       const { result } = renderHook(() => {
-        return useBackboneGetter(getFullName, {
+        return useBackboneGetter(getter, {
           object: user,
         });
       });
 
-      expect(result.current).toBe('Karl Marx');
+      expect(getter).toHaveBeenCalled();
+
+      expect(result.current).toBe('Carlos Marcos');
+    });
+  });
+
+  describe('when watching an event', () => {
+    it('executes the getter when event is triggered', () => {
+      const getter = jest.fn(getFullName);
+
+      const { result } = renderHook(() => {
+        return useBackboneGetter(getter, {
+          object: user,
+          watch: {
+            events: ['rename'],
+          },
+        });
+      });
+
+      expect(getter).toHaveBeenCalledTimes(1);
+
+      act(() => {
+        user.set('firstName', 'Frederico');
+        user.set('lastName', 'dos Anjos');
+        user.trigger('rename');
+      });
+
+      expect(getter).toHaveBeenCalledTimes(2);
+
+      expect(result.current).toBe('Frederico dos Anjos');
+    });
+  });
+
+  describe('when watching a list of values', () => {
+    type CalcModel = Model<{ operation: '+' | '-' }>;
+
+    const object: CalcModel = new Model({ operation: '+' });
+
+    const calc = (calc: CalcModel, valueA: number, valueB: number) => {
+      return calc.get('operation') === '+' ? valueA + valueB : valueA - valueB;
+    };
+
+    it('pass the values as getter parameters', () => {
+      const getter = jest.fn(calc);
+
+      renderHook(() => {
+        return useBackboneGetter(getter, {
+          object,
+          watch: {
+            values: [1, 2],
+          },
+        });
+      });
+
+      expect(getter).toHaveBeenCalledWith(object, 1, 2);
+    });
+
+    it('executes the getter when values changes', () => {
+      const getter = jest.fn(calc);
+
+      const { rerender, result } = renderHook(
+        (values: [number, number]) => {
+          return useBackboneGetter(getter, {
+            object,
+            watch: {
+              values,
+            },
+          });
+        },
+        { initialProps: [3, 4] },
+      );
+
+      expect(getter).toHaveBeenCalledTimes(1);
+
+      rerender([5, 6]);
+
+      expect(getter).toHaveBeenCalledTimes(2);
+
+      expect(result.current).toBe(11);
     });
   });
 });
