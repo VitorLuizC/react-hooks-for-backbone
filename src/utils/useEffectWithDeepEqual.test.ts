f012cfa0f8dc69bafc40050a07c896ca59b54f2c
Vitor L Cavalcanti <vitorluizc@outlook.com>
2022-08-04 18:09:50 -0300

    :sparkles: create useEffectWithDeepEqual hook

---
+++ src/utils/useEffectWithDeepEqual.test.ts
@@ -0,0 +1,42 @@
+import { renderHook } from '@testing-library/react';
+import useEffectWithDeepEqual from './useEffectWithDeepEqual.js';
+
+describe('useEffectWithDeepEqual', () => {
+  it('only executes effect if dependencies array change', () => {
+    const effect = jest.fn();
+
+    const { rerender } = renderHook(
+      (object) => useEffectWithDeepEqual(effect, [object]),
+      {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        initialProps: { id: 1 } as any,
+      },
+    );
+
+    expect(effect).toHaveBeenCalledTimes(1);
+
+    rerender({
+      id: 1,
+    });
+
+    rerender({
+      id: 1,
+      other: {
+        name: 'Numbers',
+        numbers: [1, 2, 3],
+      },
+    });
+
+    expect(effect).toHaveBeenCalledTimes(2);
+
+    rerender({
+      id: 1,
+      other: {
+        name: 'Numbers',
+        numbers: [1, 2, 3],
+      },
+    });
+
+    expect(effect).toHaveBeenCalledTimes(2);
+  });
+});
---
+++ src/utils/useEffectWithDeepEqual.ts
@@ -0,0 +1,23 @@
+import { EffectCallback, useEffect, useRef } from 'react';
+import equal from 'fast-deep-equal';
+
+/**
+ * React.js Hook that runs an effect but compare dependencies using the
+ * `fast-deep-equal` algorithm.
+ */
+function useEffectWithDeepEqual(
+  effect: EffectCallback,
+  dependencies: unknown[],
+) {
+  const previousDependenciesRef = useRef<unknown[]>();
+
+  useEffect(() => {
+    if (equal(dependencies, previousDependenciesRef.current)) return;
+
+    previousDependenciesRef.current = dependencies;
+
+    return effect();
+  }, dependencies);
+}
+
+export default useEffectWithDeepEqual;
