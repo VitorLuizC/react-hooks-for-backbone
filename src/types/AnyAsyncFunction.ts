3e8e353c61e5f9ecdeaa62970a6771164202526c
Vitor L Cavalcanti <vitorluizc@outlook.com>
2023-05-28 23:35:26 -0300

    🏷️ create AnyAsyncFunction type

---
+++ src/types/AnyAsyncFunction.ts
@@ -0,0 +1,9 @@
+/**
+ * Function that has any context, arguments, and result in a 'PromiseLike'.
+ *
+ * Similar to {@link AnyFunction}.
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type AnyAsyncFunction = (this: any, ...args: any) => PromiseLike<any>;
+
+export default AnyAsyncFunction
